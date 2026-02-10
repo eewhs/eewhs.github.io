d8ff85b9cbf17cd3b8dd5d335bc194c1650d5bb7
HiroshimaCam <ooitomoya@gmail.com>
2026-02-10 19:05:31 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-
+function getJmaEqinfo() {
+    fetch('https://www.jma.go.jp/bosai/quake/data/list.json').then(r=>r.json()).then(r=>{
+        document.getElementById('eqinfotable').innerHTML = "";
+        let num = -1;
+        for (let i=0; i<50; i++) {
+            num++;
+            const content = r[num];
+            if (content.ttl !== "震源・震度情報") {
+                i--;
+                continue;
+            }
+            const maxint = content.maxi.replace('-', '弱').replace('+', '強');
+            const hypo = content.anm;
+            const time = content.at.substr(8, 8).replace('T', '日').replace(':', '時') + "分　";
+            document.getElementById('eqinfotable').innerHTML += `${time}最大震度${maxint} ${hypo}<br>`;
+        }
+    })
+}
+getJmaEqinfo();
